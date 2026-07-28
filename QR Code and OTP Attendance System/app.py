ca8d74edd2be45f73f673dde4dfe30d5ae830a2e
Sumit Saxena <sumitsaxena2716@gmail.com>
2026-07-28 14:54:27 +0530

    Update app.py

--- QR Code and OTP Attendance System/app.py
+++ QR Code and OTP Attendance System/app.py
@@ -12,6 +12,7 @@ from openpyxl.styles import PatternFill, Font, Alignment
 
 app = Flask(__name__)
 
+
 # ================= CONFIG =================
 TEACHER_USERNAME = "Admin"
 TEACHER_PASSWORD = "admin@123"
